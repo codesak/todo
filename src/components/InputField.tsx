0c4b9f201040b98f7bc8fb3ff5ab9540632276e9
codesak <sakshamverma7844@gmail.com>
2023-01-03 00:44:24 +0530

    yes

---
+++ src/components/InputField.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputField: React.FC = () => {
+  return (
+    <div>: React.FC</div>
+  )
+}
+
+export default InputField
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleTodo = () => {
+  return (
+    <div>SingleTodo</div>
+  )
+}
+
+export default SingleTodo
