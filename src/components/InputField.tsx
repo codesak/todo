70cbe4cfaae4e99ac971e76a68d27ea7f80769e2
codesak <sakshamverma7844@gmail.com>
2023-01-03 20:00:51 +0530

    all done

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,33 @@
-import React from "react";
-import "../components/styles.scss";
+import React, { useRef } from "react";
+import "./styles.scss";
 
-interface props{
-  todo:string;
-  setTodo:React.Dispatch<React.SetStateAction<string>>;
-  handleAdd:(e:React.FormEvent)=>void;
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<props> = ({todo, setTodo, handleAdd}) => {
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input"
-    onSubmit={(e)=>handleAdd(e)}>
-      <input type="text" placeholder="Enter a Task" value={todo} onChange={(e)=>setTodo(e.target.value)} className="input__box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
       <button type="submit" className="input_submit">
-        Go
+        GO
       </button>
     </form>
   );
